8acf5a32b46e9a35ac9ad744a6825611b9219a83
bcorsak <boris.cors@gmail.com>
2018-05-10 17:30:22 +0200

    Case 464. Update client for the new 'reply to email' field.

--- libs/Branding.php
+++ libs/Branding.php
@@ -8,20 +8,28 @@ namespace DigiSigner;
  
 class Branding extends ExportObject {
 
-  protected $email_from_field = null;
-	
-	protected $exportProps = array('email_from_field');
-	 
-	public function setEmailFromField($email_from_field) {
-		$this->email_from_field = $email_from_field;
-	}
-	
-	public function getEmailFromField() {
-		return $this->email_from_field;
-	}
-	
-	 protected function getCollectionsMap() {
-    return array(); //doesn't hold any collections in it's props
-  }
-	
+    protected $email_from_field = null;
+    protected $reply_to_email = null;
+
+    protected $exportProps = array('email_from_field', 'reply_to_email');
+
+    public function setEmailFromField($email_from_field) {
+        $this->email_from_field = $email_from_field;
+    }
+
+    public function getEmailFromField() {
+        return $this->email_from_field;
+    }
+
+    public function getReplyToEmail() {
+        return $this->reply_to_email;
+    }
+
+    public function setReplyToEmail($reply_to_email) {
+        $this->reply_to_email = $reply_to_email;
+    }
+
+    protected function getCollectionsMap() {
+        return array(); //doesn't hold any collections in it's props
+    }
 }
