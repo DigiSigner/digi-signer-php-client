1a1a4a672fa66f58d7bcefbf506f47ce4cd9158b
Dmitry Lakhin <dmitry.lakhin@digisigner.com>
2019-08-07 10:05:52 +0200

    Removed option to select how to send a signature request: via email or SMS. This option is not supported by the backend any more.

--- libs/Signer.php
+++ libs/Signer.php
@@ -6,15 +6,12 @@ class Signer extends ExportObject {
 	protected $email = '';
 	protected $role = null;
 	protected $order = null;
-	protected $send_email = null;   // can be true or false
-	protected $send_sms = null;     // can be true or false
-	protected $phone_number = null;
 	protected $fields = array();
 	protected $existing_fields = array();
 	protected $is_signature_completed = null;
 	protected $sign_document_url = null;
 
-	protected $exportProps = array('email', 'role', 'order', 'send_email', 'send_sms', 'phone_number', 'fields', 'existing_fields');
+	protected $exportProps = array('email', 'role', 'order', 'fields', 'existing_fields');
 
 	public function __construct($email = '') {
 		$this->email = $email;
@@ -39,30 +36,6 @@ class Signer extends ExportObject {
 
     public function getOrder() {
         return $this->order;
-    }
-
-    public function getSendEmail() {
-        return $this->send_email;
-    }
-
-    public function setSendEmail($send_email) {
-        $this->send_email = $send_email;
-    }
-
-    public function getSendSms() {
-        return $this->send_sms;
-    }
-
-    public function setSendSms($send_sms) {
-        $this->send_sms = $send_sms;
-    }
-
-    public function getPhoneNumber() {
-        return $this->phone_number;
-    }
-
-    public function setPhoneNumber($phone_number) {
-        $this->phone_number = $phone_number;
     }
 
 	public function addField(Field $field) {
